f17e39e68adefd95a42dff3122b924a79fc80005
cmm6758 <118139822+cmm6758@users.noreply.github.com>
2023-12-06 16:06:42 -0600

    Update shared_instances.py

--- ROS_Voicer_Packages/utari10_jackal/ROS_Voicer_Python_Files/shared_instances.py
+++ ROS_Voicer_Packages/utari10_jackal/ROS_Voicer_Python_Files/shared_instances.py
@@ -1,3 +1,8 @@
 from zedcam import ZedCam
 
+# shared_instances.py - dummy script to import ZedCam to main script and picovoicer.py while avoiding circular import error
+
+# Carlos Mella-Rijo
+# Dynamic Networks and Control Laboratory
+# University of Texas at Arlington, Department of Electrical Engineering
 zc = ZedCam()
